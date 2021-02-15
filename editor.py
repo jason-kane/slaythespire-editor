9b3a4bea89b3e8fa850c61e7dea49cb4709ec1bb
Jason Kane <jason.w.kane@gmail.com>
2021-02-14 20:19:47 -0800

    minor tweak, daily doesn't have neow choices

--- editor.py
+++ editor.py
@@ -56,6 +56,7 @@ with zipfile.ZipFile("../desktop-1.0.jar", 'r') as zf:
 
             if reg:
                 NEOW_CHOICES.append(reg.groups()[0])
+        NEOW_CHOICES.append("")  # yeah, for real.
     else:
         print('neow data is not available')
 
